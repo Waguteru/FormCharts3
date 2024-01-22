d2f89d0cc0e1408d9452449f86c3f048c7b4fdfa
Настя Жукова <zhukova.nastya2018@yandex.ru>
2024-01-22 14:06:50 +0300

    Добавьте файлы проекта.

---
+++ FormCharts3/Form1.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace FormCharts3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.producttblBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.database1DataSet3 = new FormCharts3.Database1DataSet3();
+            this.cartesianChart1 = new LiveCharts.WinForms.CartesianChart();
+            this.notificationEventHandlerBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.database1DataSet = new FormCharts3.Database1DataSet();
+            this.productstblBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.products_tblTableAdapter = new FormCharts3.Database1DataSetTableAdapters.products_tblTableAdapter();
+            this.database1DataSet1 = new FormCharts3.Database1DataSet1();
+            this.tableBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.tableTableAdapter = new FormCharts3.Database1DataSet1TableAdapters.TableTableAdapter();
+            this.database1DataSet2 = new FormCharts3.Database1DataSet2();
+            this.producttblBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.product_tblTableAdapter = new FormCharts3.Database1DataSet2TableAdapters.Product_tblTableAdapter();
+            this.product_tblTableAdapter1 = new FormCharts3.Database1DataSet3TableAdapters.Product_tblTableAdapter();
+            this.articleDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nameproductsDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.price_products = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataBaseBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.form1BindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.toolStrip1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.producttblBindingSource1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.database1DataSet3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.notificationEventHandlerBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.database1DataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.productstblBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.database1DataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tableBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.database1DataSet2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.producttblBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataBaseBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.form1BindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButton1.Image")));
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(68, 22);
+            this.toolStripButton1.Text = "построить";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 43.875F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 56.125F));
+            this.tableLayoutPanel1.Controls.Add(this.dataGridView1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.cartesianChart1, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 25);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 425);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.articleDataGridViewTextBoxColumn,
+            this.nameproductsDataGridViewTextBoxColumn,
+            this.price_products});
+            this.dataGridView1.DataSource = this.producttblBindingSource1;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 3);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(345, 419);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // producttblBindingSource1
+            // 
+            this.producttblBindingSource1.DataMember = "Product_tbl";
+            this.producttblBindingSource1.DataSource = this.database1DataSet3;
+            // 
+            // database1DataSet3
+            // 
+            this.database1DataSet3.DataSetName = "Database1DataSet3";
+            this.database1DataSet3.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // cartesianChart1
+            // 
+            this.cartesianChart1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.cartesianChart1.Location = new System.Drawing.Point(354, 3);
+            this.cartesianChart1.Name = "cartesianChart1";
+            this.cartesianChart1.Size = new System.Drawing.Size(443, 419);
+            this.cartesianChart1.TabIndex = 1;
+            this.cartesianChart1.Text = "cartesianChart1";
+            // 
+            // notificationEventHandlerBindingSource
+            // 
+            this.notificationEventHandlerBindingSource.DataSource = typeof(Npgsql.NotificationEventHandler);
+            // 
+            // database1DataSet
+            // 
+            this.database1DataSet.DataSetName = "Database1DataSet";
+            this.database1DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // productstblBindingSource
+            // 
+            this.productstblBindingSource.DataMember = "products_tbl";
+            this.productstblBindingSource.DataSource = this.database1DataSet;
+            // 
+            // products_tblTableAdapter
+            // 
+            this.products_tblTableAdapter.ClearBeforeFill = true;
+            // 
+            // database1DataSet1
+            // 
+            this.database1DataSet1.DataSetName = "Database1DataSet1";
+            this.database1DataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // tableBindingSource
+            // 
+            this.tableBindingSource.DataMember = "Table";
+            this.tableBindingSource.DataSource = this.database1DataSet1;
+            // 
+            // tableTableAdapter
+            // 
+            this.tableTableAdapter.ClearBeforeFill = true;
+            // 
+            // database1DataSet2
+            // 
+            this.database1DataSet2.DataSetName = "Database1DataSet2";
+            this.database1DataSet2.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // producttblBindingSource
+            // 
+            this.producttblBindingSource.DataMember = "Product_tbl";
+            this.producttblBindingSource.DataSource = this.database1DataSet2;
+            // 
+            // product_tblTableAdapter
+            // 
+            this.product_tblTableAdapter.ClearBeforeFill = true;
+            // 
+            // product_tblTableAdapter1
+            // 
+            this.product_tblTableAdapter1.ClearBeforeFill = true;
+            // 
+            // articleDataGridViewTextBoxColumn
+            // 
+            this.articleDataGridViewTextBoxColumn.DataPropertyName = "article";
+            this.articleDataGridViewTextBoxColumn.HeaderText = "article";
+            this.articleDataGridViewTextBoxColumn.Name = "articleDataGridViewTextBoxColumn";
+            this.articleDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // nameproductsDataGridViewTextBoxColumn
+            // 
+            this.nameproductsDataGridViewTextBoxColumn.DataPropertyName = "name_products";
+            this.nameproductsDataGridViewTextBoxColumn.HeaderText = "name_products";
+            this.nameproductsDataGridViewTextBoxColumn.Name = "nameproductsDataGridViewTextBoxColumn";
+            this.nameproductsDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // price_products
+            // 
+            this.price_products.DataPropertyName = "price_products";
+            this.price_products.HeaderText = "price_products";
+            this.price_products.Name = "price_products";
+            this.price_products.ReadOnly = true;
+            // 
+            // dataBaseBindingSource
+            // 
+            this.dataBaseBindingSource.DataSource = typeof(FormCharts.DataBase);
+            // 
+            // form1BindingSource
+            // 
+            this.form1BindingSource.DataSource = typeof(FormCharts3.Form1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.producttblBindingSource1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.database1DataSet3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.notificationEventHandlerBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.database1DataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.productstblBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.database1DataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tableBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.database1DataSet2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.producttblBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataBaseBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.form1BindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private LiveCharts.WinForms.CartesianChart cartesianChart1;
+        private System.Windows.Forms.BindingSource dataBaseBindingSource;
+        private System.Windows.Forms.BindingSource notificationEventHandlerBindingSource;
+        private System.Windows.Forms.BindingSource form1BindingSource;
+        private Database1DataSet database1DataSet;
+        private System.Windows.Forms.BindingSource productstblBindingSource;
+        private Database1DataSetTableAdapters.products_tblTableAdapter products_tblTableAdapter;
+        private Database1DataSet1 database1DataSet1;
+        private System.Windows.Forms.BindingSource tableBindingSource;
+        private Database1DataSet1TableAdapters.TableTableAdapter tableTableAdapter;
+        private Database1DataSet2 database1DataSet2;
+        private System.Windows.Forms.BindingSource producttblBindingSource;
+        private Database1DataSet2TableAdapters.Product_tblTableAdapter product_tblTableAdapter;
+        private Database1DataSet3 database1DataSet3;
+        private System.Windows.Forms.BindingSource producttblBindingSource1;
+        private Database1DataSet3TableAdapters.Product_tblTableAdapter product_tblTableAdapter1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn articleDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameproductsDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn price_products;
+    }
+}
+
---
+++ FormCharts3/Form1.cs
@@ -0,0 +1,85 @@
+﻿using FormCharts;
+using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using LiveCharts;
+using LiveCharts.Wpf;
+
+
+namespace FormCharts3
+{
+    public partial class Form1 : Form
+    {
+        
+       
+        public Form1()
+        {
+            InitializeComponent();
+            StartPosition = FormStartPosition.CenterScreen;
+        }
+        
+
+       
+
+
+
+
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            SeriesCollection series = new SeriesCollection();
+            
+            ChartValues<int> priceValues = new ChartValues<int>();
+
+            List<string> name_products = new List<string>();
+
+            foreach (var priceRow in database1DataSet3.Product_tbl)
+            {
+                priceValues.Add(priceRow.price_products);
+
+                name_products.Add(priceRow.name_products.ToString());
+            }
+
+            cartesianChart1.AxisX.Clear();
+
+            cartesianChart1.AxisX.Add(new Axis()
+            {
+                Title = "названия продуктов",
+                Labels = name_products
+            });
+
+            LineSeries priceLine = new LineSeries();
+
+            priceLine.Title = "цена";
+            priceLine.Values = priceValues;
+
+            series.Add(priceLine);
+
+            cartesianChart1.Series = series;
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "database1DataSet3.Product_tbl". При необходимости она может быть перемещена или удалена.
+            this.product_tblTableAdapter1.Fill(this.database1DataSet3.Product_tbl);
+          
+           cartesianChart1.LegendLocation = LegendLocation.Bottom;
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
